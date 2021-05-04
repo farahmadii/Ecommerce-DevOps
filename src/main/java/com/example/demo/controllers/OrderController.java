b63131353801d3200d23755d66229786eff5bd69
farahmadii <33867973+farahmadii@users.noreply.github.com>
2021-05-04 12:34:28 +0200

    adding logs in info level to be used later by splunk

--- src/main/java/com/example/demo/controllers/OrderController.java
+++ src/main/java/com/example/demo/controllers/OrderController.java
@@ -2,6 +2,9 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import com.example.demo.model.persistence.Cart;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,17 +13,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.persistence.Cart;
 import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.UserOrder;
-import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.OrderRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
-	
+
+	private final static Logger logger = LoggerFactory.getLogger(OrderController.class);
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -31,21 +33,34 @@ public class OrderController {
 	
 	@PostMapping("/submit/{username}")
 	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
+		logger.info("Attempting to submit order for user: " + username);
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			logger.info("submit request failed, Reason: user not found.");
 			return ResponseEntity.notFound().build();
+		}
+		Cart cart = user.getCart();
+		if(cart.getItems().size() == 0) {
+			logger.info("submit request failed, Reason: cart is empty.");
+			return ResponseEntity.badRequest().build();
 		}
 		UserOrder order = UserOrder.createFromCart(user.getCart());
 		orderRepository.save(order);
+		user.setCart(new Cart());
+		userRepository.save(user);
+		logger.info("order places successfully for user: {}, response code: 200" + username);
 		return ResponseEntity.ok(order);
 	}
 	
 	@GetMapping("/history/{username}")
 	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
+		logger.info("Attempting to get order history for user: " + username);
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			logger.info("get history request failed, Reason: user not found.");
 			return ResponseEntity.notFound().build();
 		}
+		logger.info("get history successfully done for user: {}, response code: 200" + username);
 		return ResponseEntity.ok(orderRepository.findByUser(user));
 	}
 }
